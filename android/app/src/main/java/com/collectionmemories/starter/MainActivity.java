ad0b9a52548535dad11d22048043ea27be374e4a
Kevin Espejel <kevin.espejelmtz@gmail.com>
2022-04-02 13:03:51 -0600

    app ionic android

---
+++ android/app/src/main/java/com/collectionmemories/starter/MainActivity.java
@@ -0,0 +1,5 @@
+package com.collectionmemories.starter;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,9 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'app-movil',
-  webDir: 'dist',
+  appId: 'com.collectionmemories.starter',
+  appName: 'Collection Memories',
+  webDir: 'public',
   bundledWebRuntime: false
 };
 
